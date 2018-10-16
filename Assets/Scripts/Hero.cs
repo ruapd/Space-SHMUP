1ea462333be2b678f9d72cc25619bc5ca6f70d20
Pavel Aparcana <gpaparcana@gmail.com>
2018-10-16 16:10:44 -0400

    SHip and SHield

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hero : MonoBehaviour {
+public class Hero : MonoBehaviour 
+{
+    static public Hero S;
+    [Header("Set in Inspector")]
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
 
-	// Use this for initialization
-	void Start () {
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+    private void Awake()
+    {
+        if( S == null)
+        {
+            S = this; //set the singleton
+        }
+        else{
+            Debug.LogError("Hero.Awake() - Attmepted to assign second Hero.S!");
+        }
+    }
+    // Use this for initialization
+    void Start () 
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update () 
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        //chage transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        //rotate the ship to make it feel more dynamic.
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+    }
 }
