3b8480b2a46cdf1fe21e6b195b5762f1bff89b85
Pavel Aparcana <gpaparcana@gmail.com>
2018-10-16 22:32:54 -0400

    SpawnEnemies
    This update is able tospawn enemy_0 at random times and locations.

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -9,8 +9,41 @@ public class Main : MonoBehaviour
 
     [Header("Set in Inspector")]
     public GameObject[] prefabEnemies;
-	// Use this for initialization
-	void Start () {
+    public float enemySpawnPerSecond = 0.5f;
+    public float enemyDefaultPadding = 1.5f;
+
+    private BoundsCheck bndCheck;
+
+    void Awake()
+    {
+        S = this;
+        bndCheck = GetComponent<BoundsCheck>();
+
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+    }
+
+    public void SpawnEnemy()
+    {
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
+
+        float enemyPadding = enemyDefaultPadding;
+        if (go.GetComponent<BoundsCheck>() !=null)
+        {
+            enemyPadding = Mathf.Abs(go.GetComponent<BoundsCheck>().radius);
+        }
+
+        Vector3 pos = Vector3.zero;
+        float xMin = -bndCheck.camWidth + enemyPadding;
+        float xMax = bndCheck.camWidth - enemyPadding;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = bndCheck.camHeight + enemyPadding;
+        go.transform.position = pos;
+
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+    }
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
