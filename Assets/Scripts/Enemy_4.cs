5675bd3131ce9e44199aa2b7a81fdbbb859c5d4b
Pavel Aparcana <gpaparcana@gmail.com>
2018-10-25 00:34:44 -0400

    enemy4
    up and down

--- Assets/Scripts/Enemy_4.cs
+++ Assets/Scripts/Enemy_4.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_4 : MonoBehaviour {
-
+/// <summary>
+/// Enemy_4 will start offscreen and hten pick a random point on screen to move to.
+/// once it has arrived, it will pick another randompoint and continue until
+/// player has shot it down
+/// </summary>
+public class Enemy_4 : Enemy 
+{
+    private Vector3 p0, p1;
+    private float timeStart;
+    private float duration = 4;
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+    {
+        p0 = p1 = pos;
+
+        InitMovement();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void InitMovement()
+    {
+        p0 = p1;
+
+        float widMinRad = bndCheck.camWidth = bndCheck.radius;
+        float hgtMinRad = bndCheck.camHeight - bndCheck.radius;
+        p1.x = Random.Range(-widMinRad, widMinRad);
+        p1.y = Random.Range(-hgtMinRad, hgtMinRad);
+
+        timeStart = Time.time;
+    }
+
+    public override void Move()
+    {
+        float u = (Time.time - timeStart) / duration;
+
+        if (u>=1)
+        {
+            InitMovement();
+            u = 0;
+        }
+        u = 1 - Mathf.Pow(1 - u, 2);
+        pos = (1 - u) * p0 + u * p1;
+    }
 }
